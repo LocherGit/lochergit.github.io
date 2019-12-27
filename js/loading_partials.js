b3f02815c663d980300eee8a52819f49bfaff777
Ton_Home <nt023@hdm-stuttgart.de>
2019-12-27 14:30:06 +0100

    console logging

--- js/loading_partials.js
+++ js/loading_partials.js
@@ -4,6 +4,7 @@ $(function() {
   var includes = $("[data-include]");
   jQuery.each(includes, function() {
     var file = "./html_partials/" + $(this).data("include") + ".html";
+    console.log(file);
     $(this).load(file);
   });
 });
