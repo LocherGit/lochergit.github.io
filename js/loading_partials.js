2819f15e4bdf2d180190db49c67019765c66536f
Ton_Home <nt023@hdm-stuttgart.de>
2019-12-27 14:22:51 +0100

    attempt worked! now going a step further.

--- js/loading_partials.js
+++ js/loading_partials.js
@@ -3,7 +3,7 @@ $(function() {
   console.log("Loading partials");
   var includes = $("[data-include]");
   jQuery.each(includes, function() {
-    var file = "html_partials/" + $(this).data("include") + ".html";
+    var file = "/html_partials/" + $(this).data("include") + ".html";
     $(this).load(file);
   });
 });
