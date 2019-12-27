45f467bba2471487bff0f8b9f880c2967175218a
Ton_Home <nt023@hdm-stuttgart.de>
2019-12-27 23:19:38 +0100

    restructuring,  transcript chapters will now use same partial loading script

--- js/loading_partials.js
+++ js/loading_partials.js
@@ -1,6 +1,5 @@
 /* Loading Partials */
 $(function() {
-  console.log("Loading partials");
   var includes = $("[data-include]");
   jQuery.each(includes, function() {
     var file = "/html_partials/" + $(this).data("include") + ".html";
