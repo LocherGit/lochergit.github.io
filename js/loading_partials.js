7e4614a7b4d0bf2de1546821e5e88bf55fd74272
Ton_Home <nt023@hdm-stuttgart.de>
2019-12-27 15:10:11 +0100

    Trying a squash commit

--- js/loading_partials.js
+++ js/loading_partials.js
@@ -3,8 +3,7 @@ $(function() {
   console.log("Loading partials");
   var includes = $("[data-include]");
   jQuery.each(includes, function() {
-    var file = "./html_partials/" + $(this).data("include") + ".html";
-    console.log(file);
+    var file = "/html_partials/" + $(this).data("include") + ".html";
     $(this).load(file);
   });
 });
